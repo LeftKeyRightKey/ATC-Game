df3d482e101c8e0b85ea17447ced55f7a65bf08d
RYONARU <72093608+RYONARU@users.noreply.github.com>
2022-12-26 14:51:38 +0700

    Update PlaneScript.cs

--- Assets/PlaneScript.cs
+++ Assets/PlaneScript.cs
@@ -18,6 +18,11 @@ public class PlaneScript : MonoBehaviour
         approachEpoch5,
         approachFinalEpoch
     };
+    public float speed;
+    public int MaxIndex;
+    int Pindex;
+    Transform movepoint;
+    public Transform[] points; 
 
     public PlaneState planeState = new PlaneState();
 
@@ -25,6 +30,8 @@ public class PlaneScript : MonoBehaviour
     {
         //To move object in UI, use RectTransform instead of Transform.
         planeTransform = gameObject.GetComponent<RectTransform>();
+        Pindex = 0;
+        movepoint = points[Pindex];
     }
 
     void Update()
@@ -36,6 +43,16 @@ public class PlaneScript : MonoBehaviour
 
     void AIBehaviour()
     {
-
+        Transform.position = Vector3.MoveTowards(Transform.position,movepoint.position, speed * deltaTime)
+        if(Vector3.distance(Transform.position,movepoint.position)<=0){
+            if(Pindex>MaxIndex){
+                Pindex = 0;
+            }
+        }
+        Pindex++;
+        movepoint = points[Pindex];
     }
+    Vector3 pos = movepoint.position - Transform.position;
+    float angle = MathF.Atan2(float y, float x) * MathF.Rad2Deg;
+    Transform.rotation = Quaternion.Euler(0,0,angle -90);
 }
